43f8dab09a23c51cb8e95b451ed5e40f992a3fe3
Sirajudeen <sirajudeen.yasin@gmail.com>
2021-01-12 16:23:35 +0000

    Fix treasuremap deployment issues.
    * Fixes validate_docs for treasuremap
    * Align the secret encryption/decryption changes of airshipctl
    * Aligning the manifest changes of airshipctl.
    * Added HWCC.
    * Replaced helm-operator with flux-helm
    * Updated node parameters.
    
    Relates-To: #77
    Closes: #95
    
    Change-Id: I30408de468ac2794703b95a2afc6191908567816

---
+++ tools/deployment/23_generate_secrets.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -xe
+
+: ${AIRSHIPCTL_PROJECT:="../airshipctl"}
+: ${TREASUREMAP_PROJECT:="$(pwd)"}
+
+export AIRSHIP_SITE_NAME=${AIRSHIP_SITE_NAME:-"test-site"}
+export AIRSHIP_CONFIG_METADATA_PATH=${AIRSHIP_CONFIG_METADATA_PATH:-"treasuremap/manifests/site/$AIRSHIP_SITE_NAME/metadata.yaml"}
+# Setting the same value as targetPath that gets updated after create config step (22_test_configs.sh)
+export AIRSHIP_CONFIG_MANIFEST_DIRECTORY=${AIRSHIP_CONFIG_MANIFEST_DIRECTORY:-"/tmp/default"}
+
+# Primary repo options
+export AIRSHIP_CONFIG_PHASE_REPO_URL=${AIRSHIP_CONFIG_PHASE_REPO_URL:-"https://review.opendev.org/airship/treasuremap"}
+
+cd ${AIRSHIPCTL_PROJECT}
+./tools/deployment/23_generate_secrets.sh
