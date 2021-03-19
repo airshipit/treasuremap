89fc29184aed4193490013c31214ec5c5a092d97
James Gu <james.gu@att.com>
2021-03-19 13:13:01 -0700

    Enhanced deployment scripts to support baremetal multi-node
    Additionally aligned between Treasuremap and Airshipctl to use the same
    environment variable name "SITE" for a site name.
    
    Change-Id: Id6405b7074d34bc73d730eea76d9dd6ab7901405
    Signed-off-by: James Gu <james.gu@att.com>

--- tools/deployment/31_deploy_initinfra_target_node.sh
+++ tools/deployment/31_deploy_initinfra_target_node.sh
@@ -18,7 +18,19 @@ export KUBECONFIG=${KUBECONFIG:-"$HOME/.airship/kubeconfig"}
 export KUBECONFIG_TARGET_CONTEXT=${KUBECONFIG_TARGET_CONTEXT:-"target-cluster"}
 : ${AIRSHIPCTL_PROJECT:="../airshipctl"}
 
+TARGET_NODE=${TARGET_NODE:-"$(airshipctl phase render controlplane-ephemeral \
+	-k BareMetalHost -l airshipit.org/k8s-role=controlplane-host \
+	2> /dev/null | \
+	yq .metadata.name | \
+	sed 's/"//g')"}
+
 cd ${AIRSHIPCTL_PROJECT}
+
+kubectl \
+  --kubeconfig $KUBECONFIG \
+  --context $KUBECONFIG_TARGET_CONTEXT \
+  --request-timeout 10s \
+  label --overwrite nodes $TARGET_NODE node-type=controlplane
 
 ./tools/deployment/31_deploy_initinfra_target_node.sh
 
