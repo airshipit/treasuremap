99b66a1d932c78ae4efcf2f5e2cc6f7f444dc02a
Sergiy Markin <smarkin@mirantis.com>
2025-07-31 18:50:24 +0000

    Update deploy-env role default setup
    Change-Id: Ib3d3aed9c9ef97f844a81a4a820003c3cb9b82b9
    Signed-off-by: Sergiy Markin <smarkin@mirantis.com>

--- tools/deployment/airskiff/developer/000-prepare-k8s.sh
+++ tools/deployment/airskiff/developer/000-prepare-k8s.sh
@@ -57,14 +57,14 @@ kubectl rollout status --watch --timeout=300s -n kube-system deployment/coredns
 PATCH=$(mktemp)
 HOSTIP=$(hostname -I| awk '{print $1}')
 kubectl get configmap coredns -n kube-system -o json | jq -r "{data: .data}"  > "${PATCH}"
-sed -i "s;forward . /etc/resolv.conf {\\\n       max_concurrent 1000\\\n    }\\\n;forward . /etc/resolv.conf {\\\n       max_concurrent 1000\\\n    }\\\nhosts {\\\n       $HOSTIP control-plane.minikube.internal\\\n       fallthrough\\\n    }\\\n;" "${PATCH}"
+echo "${PATCH}"
+sed -i "s;forward . 8.8.8.8 {\\\n      max_concurrent 1000\\\n    }\\\n;forward . 8.8.8.8 {\\\n      max_concurrent 1000\\\n    }\\\nhosts {\\\n       $HOSTIP control-plane.minikube.internal\\\n       fallthrough\\\n    }\\\n;" "${PATCH}"
+echo "${PATCH}"
 kubectl patch configmap coredns -n kube-system --patch-file "${PATCH}"
 rm -f "${PATCH}"
 kubectl rollout restart -n kube-system deployment/coredns
 kubectl rollout status --watch --timeout=300s -n kube-system deployment/coredns
 sleep 10
-
-sudo sh -c 'echo "nameserver 10.96.0.10" > /etc/resolv.conf'
 
 host -v control-plane.minikube.internal
 
