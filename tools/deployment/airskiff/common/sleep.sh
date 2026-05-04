3409f6cee207d2b9f12460cdc318eb1a8653a1bf
Sergiy Markin <smarkin@mirantis.com>
2026-05-04 16:58:00 +0000

    Update OSH dependences
    Change-Id: Ibeef00a80e77e8c896b3b0866b223f718b7d8136
    Signed-off-by: Sergiy Markin <smarkin@mirantis.com>

--- tools/deployment/airskiff/common/sleep.sh
+++ tools/deployment/airskiff/common/sleep.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 set -ex
 
-env_output=$(env)
 
-# Loop through each line of the env output
-while IFS= read -r line; do
-    # Extract the variable name and value
-    variable=$(echo "$line" | cut -d= -f1)
-    value=$(echo "$line" | cut -d= -f2-)
+: "${ENABLE_SLEEP:=false}"
 
-    # Print the export command
-    echo "export $variable=\"$value\""
-done <<< "$env_output"
+ENABLE_SLEEP=$(echo "${ENABLE_SLEEP}" | tr '[:upper:]' '[:lower:]')
 
-echo "Sleeping.............."
-while true; do sleep 10; done
+if [[ "${ENABLE_SLEEP}" == "true" ]]; then
+
+    env_output=$(env)
+
+    # Loop through each line of the env output
+    while IFS= read -r line; do
+        # Extract the variable name and value
+        variable=$(echo "$line" | cut -d= -f1)
+        value=$(echo "$line" | cut -d= -f2-)
+
+        # Print the export command
+        echo "export $variable=\"$value\""
+    done <<< "$env_output"
+
+    echo "Sleeping.............."
+    while true; do sleep 10; done
+fi
