70da764fc627e660c626b68395b1be407a981f1c
Sergiy Markin <smarkin@mirantis.com>
2026-05-09 00:07:58 +0000

    Make armada-operator deployment conditional on USE_ARMADA_GO
    Add USE_ARMADA_GO env variable check (default: false) to
    027-enable-armada-operator.sh so the armada config override
    is only applied when the Go operator is explicitly requested.
    
    Previously USE_ARMADA_GO was applied to bootstrap process only.
    
    Add insecure_registries var to deploy-env role
    with the value of local DOCKER_REGISTRY.
    
    Change-Id: I092ddef8343b822ff91152ae1a4fa560843bd759
    Signed-off-by: Sergiy Markin <smarkin@mirantis.com>

--- tools/deployment/airskiff/developer/027-enable-armada-operator.sh
+++ tools/deployment/airskiff/developer/027-enable-armada-operator.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 
-cp -a tools/gate/manifests/armada.yaml global/software/charts/ucp/armada/armada.yaml
+: "${USE_ARMADA_GO:=false}"
+# Convert both values to lowercase (or uppercase)
+USE_ARMADA_GO=$(echo "$USE_ARMADA_GO" | tr '[:upper:]' '[:lower:]')
+export USE_ARMADA_GO
+
+if [[ ${USE_ARMADA_GO} = true ]] ; then
+    cp -a tools/gate/manifests/armada.yaml global/software/charts/ucp/armada/armada.yaml
+fi
